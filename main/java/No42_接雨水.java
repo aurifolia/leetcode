49b0afc092a5adcbd9331d4aaf1418717cead07e
vnelinpe <vnelinpe@sina.com>
2024-03-10 18:18:35 +0800

    20240310

---
+++ main/java/No42_接雨水.java
@@ -0,0 +1,15 @@
+/**
+ * @author vneli
+ * @since 1.0
+ */
+public class No42_接雨水 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int trap(int[] height) {
+
+    }
+}
