5e602527f527681a63b347302bea2f06b7b6fe45
Peng Dan <peng.dan@gientech.com>
2025-03-19 21:35:00 +0800

    1

---
+++ No41_缺失的第一个正数.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ *
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ *
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No41_缺失的第一个正数 {
+    public static void main(String[] args) {
+        Solution41 solution41 = new Solution41();
+        System.out.println(solution41.firstMissingPositive(new int[]{3,4,-1,1}));
+        System.out.println(solution41.firstMissingPositive(new int[]{1, 2, 0}));
+    }
+}
+
+class Solution41 {
+    public int firstMissingPositive1(int[] nums) {
+        int length = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (num > 0) {
+                set.add(num);
+            }
+        }
+        for (int i = 1; i <= length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return length + 1;
+    }
+
+    public int firstMissingPositive(int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            while (nums[i] > 0 && nums[i] <= length && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return length + 1;
+    }
+}
---
+++ No54_螺旋矩阵.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+ *
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No54_螺旋矩阵 {
+    public static void main(String[] args) {
+        Solution54 solution54 = new Solution54();
+        System.out.println(solution54.spiralOrder(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
+        System.out.println(solution54.spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}));
+    }
+}
+
+class Solution54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int sm = 0;
+        int sn = 0;
+        int m = matrix.length - 1;
+        int n = matrix[0].length - 1;
+        List<Integer> result = new ArrayList<>();
+        while (sm <= m && sn <= n) {
+            // right
+            for (int i = sn; i <= n; i++) {
+                result.add(matrix[sm][i]);
+            }
+            sm++;
+            if (sm > m) {
+                break;
+            }
+            // down
+            for (int i = sm; i <= m; i++) {
+                result.add(matrix[i][n]);
+            }
+            n--;
+            if (sn > n) {
+                break;
+            }
+            // left
+            for (int i = n; i >= sn; i--) {
+                result.add(matrix[m][i]);
+            }
+            m--;
+            if (sm > m) {
+                break;
+            }
+            // up
+            for (int i = m; i >= sm; i--) {
+                result.add(matrix[i][sn]);
+            }
+            sn++;
+        }
+        return result;
+    }
+}
