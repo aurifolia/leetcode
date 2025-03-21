e4fd821fb079e707309c6ffc29a2af6a40b5916e
Peng Dan <peng.dan@gientech.com>
2025-03-21 22:27:30 +0800

    1

---
+++ No141_环形链表.java
@@ -0,0 +1,27 @@
+/**
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No141_环形链表 {
+    public static void main(String[] args) {
+        Solution141 solution141 = new Solution141();
+    }
+}
+
+class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ No160_相交链表.java
@@ -0,0 +1,69 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No160_相交链表 {
+    public static void main(String[] args) {
+        Solution160 solution160 = new Solution160();
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    ListNode(int x, ListNode next) {
+        val = x;
+        this.next = next;
+    }
+}
+
+class Solution160 {
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        Set<ListNode> nodeSet = new HashSet<>();
+        ListNode node = headA;
+        while (node != null) {
+            nodeSet.add(node);
+            node = node.next;
+        }
+        node = headB;
+        while (node != null) {
+            if (nodeSet.contains(node)) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode nodeA = headA;
+        ListNode nodeB = headB;
+        while (nodeA != nodeB) {
+            if (nodeA == null) {
+                nodeA = headB;
+            }
+            else {
+                nodeA = nodeA.next;
+            }
+            if (nodeB == null) {
+                nodeB = headA;
+            }
+            else {
+                nodeB = nodeB.next;
+            }
+        }
+        return nodeA;
+    }
+}
