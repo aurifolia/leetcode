166c6ff1dacd2e649b0c86131baaca1e77e92a70
Peng Dan <peng.dan@gientech.com>
2025-04-03 18:30:31 +0800

    1

---
+++ No121_买卖股票的最佳时机.java
@@ -0,0 +1,47 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ *
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ *
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No121_买卖股票的最佳时机 {
+    public static void main(String[] args) {
+        Solution121 solution121 = new Solution121();
+        System.out.println(solution121.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+}
+
+class Solution121 {
+    public int maxProfit1(int[] prices) {
+        Deque<Integer> stack = new LinkedList<>();
+        int[] rightMax = new int[prices.length];
+        for (int i = prices.length - 1; i >= 0; i--) {
+            if (stack.isEmpty() || stack.peek() < prices[i]) {
+                stack.push(prices[i]);
+            }
+            rightMax[i] = stack.peek();
+        }
+        int max = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            max = Math.max(max, rightMax[i + 1] - prices[i]);
+        }
+        return max;
+    }
+
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+}
---
+++ No55_跳跃游戏.java
@@ -0,0 +1,54 @@
+import java.util.Deque;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+/**
+ * 给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+ *
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No55_跳跃游戏 {
+    public static void main(String[] args) {
+        Solution55 solution55 = new Solution55();
+        System.out.println(solution55.canJump(new int[]{2, 3, 1, 1, 4}));
+    }
+}
+
+class Solution55 {
+    public boolean canJump1(int[] nums) {
+        Deque<Integer> queue = new LinkedList<>();
+        queue.push(0);
+        int max = 0;
+        while (!queue.isEmpty()) {
+            Set<Integer> set = new LinkedHashSet<>();
+            for (int i = 0, size = queue.size(); i < size; i++) {
+                Integer pop = queue.pop();
+                set.remove(pop);
+                max = Math.max(max, pop + nums[pop]);
+                if (max >= nums.length - 1) {
+                    return true;
+                }
+                for (int j = 1; j <= nums[pop]; j++) {
+                    set.add(pop + j);
+                }
+            }
+            queue.addAll(set);
+        }
+        return false;
+    }
+
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+        }
+        return true;
+    }
+}
