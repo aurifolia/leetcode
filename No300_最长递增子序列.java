fb824c63b0914317c58e3f8daab3b71cd0d7815f
Peng Dan <peng.dan@gientech.com>
2025-04-06 01:08:21 +0800

    1

---
+++ No300_最长递增子序列.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ *
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ *
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No300_最长递增子序列 {
+}
+
+class Solution300 {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
---
+++ No5_最长回文子串.java
@@ -0,0 +1,76 @@
+/**
+ * 给你一个字符串 s，找到 s 中最长的 回文 子串。
+ *
+ * @author Peng Dan
+ * @since 1.0
+ */
+public class No5_最长回文子串 {
+    public static void main(String[] args) {
+        Solution5 solution5 = new Solution5();
+        System.out.println(solution5.longestPalindrome("aacabdkacaa"));
+    }
+}
+
+class Solution5 {
+    public String longestPalindrome1(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+        }
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i == 1) {
+                        dp[i][j] = 2;
+                    }
+                    else {
+                        if (dp[i + 1][j - 1] > 0) {
+                            dp[i][j] = dp[i + 1][j - 1] + 2;
+                        }
+                    }
+                    dp[j][i] = dp[i][j];
+                    if (dp[i][j] > max) {
+                        max = dp[i][j];
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public String longestPalindrome(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+        }
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (i - j == 1) {
+                        dp[i][j] = 2;
+                    }
+                    else {
+                        if (dp[j + 1][i - 1] > 0) {
+                            dp[i][j] = dp[j + 1][i - 1] + 2;
+                        }
+                    }
+                    dp[j][i] = dp[i][j];
+                    if (dp[i][j] > max) {
+                        max = dp[i][j];
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+}
